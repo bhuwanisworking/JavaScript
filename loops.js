b8b28ac5816d227bc408a271a272ea5cf1d1150f
bhuwan <bhuwankashyap5@gmail.com>
2025-07-02 21:01:46 +0530

    loops were studied

---
+++ loops.js
@@ -0,0 +1,17 @@
+for(let index=0; index<5;index++){
+    console.log(index)
+}
+let i=0;
+while(i<5){
+    console.log(`The value is ${i}`)
+    i++;
+}
+let a=1;
+
+do{
+    console.log("bhuwan")
+    a++;
+}
+while(a<0){
+}
+// so the problem with do while is that it will run once even if the condition is false
