a6a4b03bb2cef424010c080ce7694349fc30423c
bhuwan <bhuwankashyap5@gmail.com>
2025-07-02 20:57:34 +0530

    operators were studied

---
+++ operators.js
@@ -0,0 +1,16 @@
+let a=10;
+let b=12;
+if(a>b){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+let ans=(b>a)?"greater":"smaller"
+console.log(ans)
+let c=10;
+let d="10"
+console.log(c==d)
+console.log(c===d)
+/// === checks if the value and datatypes are same oe not 
+// == checks if values are same or not irrespective of the datatype
