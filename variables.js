af5a741e0b69aca33fc76a93560de6d0fa81db2d
bhuwan <bhuwankashyap5@gmail.com>
2025-07-02 20:48:15 +0530

    datatypes were studied

---
+++ variables.js
@@ -0,0 +1,12 @@
+var a=10;
+console.log(a);
+// var is not being used anywhere as it is not good to use
+var a="bhuwan";
+console.log(a)
+// see thats why it is not used anymore so we use let everywhere 
+const b=10;
+// const cant be changed
+let c=1000;
+{
+    console.log(c)
+}
