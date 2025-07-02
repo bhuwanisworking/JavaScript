e2364548a6adc54a48d702abb5fcf88c32059a14
bhuwan <bhuwankashyap5@gmail.com>
2025-07-02 21:05:07 +0530

    functions were studied

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(){
+    console.log("hello this is greet function")
+}
+greet();
+
+let ans1=function(){
+    console.log("the another function")
+}
+console.log(ans1())
+let ans2=()=>{
+    console.log("this is another function 3")
+}
+console.log(ans2())
